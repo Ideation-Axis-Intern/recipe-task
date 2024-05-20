c045b3caeb83e99484f41d083359deb0ec30df8d
joe-boadi <joeboadi1@gmail.com>
2024-05-20 19:28:36 +0000

    Update file with components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-// import { useState } from 'react'
-import Navbar from './component/Navbar'
+// import { useEffect } from 'react'
 import "../src/App.css"
+import "./utils/Fetch_data.js"
+import Navbar from './component/Navbar'
 import {  BrowserRouter, } from 'react-router-dom'
 import Home from './component/Home'
+import Recipe from './component/Recipe'
+import Footer from './component/Footer.js'
+import AdView from './component/AdView.js'
+import About from './component/About.js'
+
+
+// useEffect(() => {
+//   FetchData()
+// }, [])
+
 
 const App = () => {
   return (
@@ -10,6 +21,10 @@ const App = () => {
       <div className='bg-base-100 text-base-content'>
         <Navbar />
         <Home />
+        <Recipe/>
+        <AdView />
+        <About />
+        <Footer />
       </div>
     </BrowserRouter>
   )
