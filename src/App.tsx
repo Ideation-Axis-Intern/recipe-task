cc8ac47dce420074b175bba98c0377340e00c8c1
joe-boadi <joeboadi1@gmail.com>
2024-05-20 11:10:13 +0000

    Try deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
 import Navbar from './component/Navbar'
 import "../src/App.css"
-import { BrowserRouter } from 'react-router-dom'
+import {  BrowserRouter, } from 'react-router-dom'
 import Home from './component/Home'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename=''>
       <div className='bg-base-100 text-base-content'>
         <Navbar />
         <Home />
