2db502aa0ed59ce28516a01be1ef9e7b52a6fde6
joe-boadi <joeboadi1@gmail.com>
2024-05-23 11:28:18 +0000

    modify data

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import "../src/App.css"
-import Navbar from './component/Navbar'
-import {  BrowserRouter, } from 'react-router-dom'
-import Recipe from './component/Recipe'
-import Footer from './component/Footer.js'
-import AdView from './component/AdView.js'
-import About from './component/About.js'
-import { useEffect, useState } from "react"
-import Home from "./component/Home.js"
+import "../src/App.css";
+import { ErrorInfo, useEffect, useState } from "react";
+import {  BrowserRouter, } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home.js";
+import Recipe from "./component/Recipe";
+import AdView from "./component/AdView.js";
+import About from "./component/About.js";
+import Footer from "./component/Footer.js";
 
 const App = () => {
-  const [recipes, setRecipes] = useState([])
-  const [selectedRecipes] = useState('')
+  const [error, setError] = useState();
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipes] = useState('');
 
   useEffect(() => {
-    fetch('https://dummyjson.com/recipes')
+
+    setIsLoading(true);
+
+    try {
+      fetch('https://dummyjson.com/recipes')
       .then((res) => {
         return res.json();
       })
@@ -21,7 +27,19 @@ const App = () => {
         console.log(data);
         setRecipes(data);
       });
+      /* typescript-eslint-disable @typescript-eslint/no-explicit-any */
+    } catch (err: any) {
+      setError(err);
+    }finally{
+      setIsLoading(false);
+    }
+    
   }, []);
+  
+    if(isLoading) return <div className="items-center text-center">Loading data...</div>;
+    if(error) return <div className="items-center text-center">Error fetching data...</div>;
+
+
 
   const filteredRecipes = selectedRecipes
   ? recipes.filter((product) => (product as { categories: string }).categories === selectedRecipes)
